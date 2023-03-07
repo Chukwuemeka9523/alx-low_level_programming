9ee49fd93f67127eec439d104e4a0588b7343844
Chukwuemeka9523 <emyperere@yahoo.com>
2023-03-06 16:58:38 -0800

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,21 +8,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n,  value, check;
+	unsigned int i, j, bool;
 
-	value = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		check = 0;
-
-		for (n = 0; accept[n] != '\0'; n++)
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (accept[n] == s[i])
+			if (*(s + i) == *(accept + j))
 			{
-				value++;
-				check = 1;
+				bool = 0;
+				break;
 			}
 		}
+		if (bool == 1)
+			break;
 	}
+	return (i)
 }
